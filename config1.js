afa3c8ece9133283bf991b621277ee0cb91fb20c
echoman12 <45900323+echoman12@users.noreply.github.com>
2019-01-05 11:59:07 +0200

    Update config1.js

--- config1.js
+++ config1.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'process.env.BOT_TOKEN'; // حط توكن البوت هنا
 
-exports.PREFIX = '2';//برفكس البوت هو يلي بيشغل لاغاني
+exports.PREFIX = 'W';//برفكس البوت هو يلي بيشغل لاغاني
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
